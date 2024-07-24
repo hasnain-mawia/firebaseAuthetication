39d22783a7c7404443271967e7291755bd0aac57
hasnain-mawia <110049696+hasnain-mawia@users.noreply.github.com>
2024-07-24 11:50:41 +0500

    detailpage done

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import {useNavigate } from 'react-router-dom';
+import {Link, useNavigate } from 'react-router-dom';
 import CLoader from '../../components/CLoader';
 import { Getdata } from '../../config/firebase';
 
@@ -28,13 +28,15 @@ function Index() {
         {products.length === 0 ? <CLoader /> : 
           products.map((product:any, i:number) => {
           return(
-            <div onClick={()=> navigate(`/detail/${product.id}`)} key={i} className='shadow-2xl rounded-[10px] border-2 border-[#c9c9c9] flex flex-col items-center justify-center text-center'>
+            <Link key={i} to={`/detail/${product.title.replace()}`} state={product}>
+            <div  className='shadow-2xl rounded-[10px] border-2 border-[#c9c9c9] flex flex-col items-center justify-center text-center'>
           <img src={product.image} alt={product.title} className='overflow-hidden rounded-[10px]'/>
           <div className='py-3 px-2'>
             <h2 className='font-bold'>{product.title.slice(0,25)}</h2>
             <p className='text-[green]'>${product.price}</p>
             </div>
           </div>
+          </Link>
           )
         }
       )}  
--- src/screens/detail/index.tsx
+++ src/screens/detail/index.tsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate} from 'react-router-dom'
 import CLoader from '../../components/CLoader'
-import {collection,doc, getDocs} from "firebase/firestore"; 
 
 function Index(){
-  const params = useParams()
-  console.log(params.id)
+  const {state} = useLocation()
    const navigate = useNavigate()
    const [detailview, setDetailview] = useState<any>(null);
+   useEffect(()=>{
+    setTimeout(()=>{
+    setDetailview(state) 
+    },200)
+  })
 
-  
   return detailview === null ? <CLoader/> : (
     <div className='max-w-[1200px] mx-auto my-10'>
        <button onClick={()=> navigate(-1)} className='bg-[#0088ff] p-2 rounded-[30px] text-white' >BACK</button>
        <div className='grid grid-cols-1 sm:grid-cols-2'>
             <div>
-                <img className='w-[300px] mx-auto' src={detailview.image} alt="" />
+                <img className='w-[400px] mx-auto' src={detailview.image} alt="" />
             </div>
             <div>
-                <h2 className='font-bold text-[18px]'>{detailview.title}</h2>
-                <p >{detailview.description}</p>
+                <h2 className='font-bold text-[18px] border-b-[1px] border-[black] pb-3'>{detailview.title}</h2>
+                <p className='my-5'>{detailview.description}</p>
                 <p className='text-[red] font-bold'>Category: {detailview.category}</p>
-                <p className='text-[green] font-bold'>${detailview.price}</p>
-                <p className=''>{detailview.description}</p>
-                <button onClick={()=>alert(detailview.title)} className='bg-[#0088ff] px-4 py-2 rounded-[30px] text-white mt-10'>Add to Cart</button>
+                <p className='text-[green] font-bold text-[22px]'>${detailview.price}</p>
+                <button onClick={()=>alert(detailview.title)} className='bg-[#0088ff] px-4 py-2 rounded-[30px] text-white mt-5'>Add to Cart</button>
             </div>
         </div>
   
